3c46ed609921723f0b97d00b1d049ed75ff45755
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-04-13 14:08:02 +0300

    Added Calculator component and its visual representation (markup, styles)

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,27 @@
+function Calculator() {
+    return (
+        <div className='calculator' id='calc'>
+            <div className='display'></div>
+            <span className='c neumorphic'>C</span>
+            <span className='signed neumorphic'>+/-</span>
+            <span className='percent neumorphic'>%</span>
+            <span className='divide neumorphic'>÷</span>
+            <span className='seven neumorphic'>7</span>
+            <span className='eight neumorphic'>8</span>
+            <span className='nine neumorphic'>9</span>
+            <span className='times neumorphic'>x</span>
+            <span className='four neumorphic'>4</span>
+            <span className='five neumorphic'>5</span>
+            <span className='six neumorphic'>6</span>
+            <span className='minus neumorphic'>-</span>
+            <span className='one neumorphic'>1</span>
+            <span className='two neumorphic'>2</span>
+            <span className='three neumorphic'>3</span>
+            <span className='plus neumorphic'>+</span>
+            <span className='zero neumorphic'>0</span>
+            <span className='decimal neumorphic'>.</span>
+            <span className='equals neumorphic'>=</span>
+        </div>
+    );
+}
+export default Calculator;

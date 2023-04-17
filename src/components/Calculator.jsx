ed6f6bd32cb98bb0786af4a1a5e94e728fa18a42
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-04-17 12:55:09 +0300

    Added functionality for working with a calculator through a computer keyboard

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,101 @@
 import React, { useState, useEffect } from 'react';
 import * as math from 'mathjs';
 
+// при нажатии на Enter несколько раз при каком-то готовом значении выдает ошибку на 56 строке
+
 const Calculator = () => {
     const [inputVal, setInputVal] = useState('');
     const operators = ['+', '-', 'x', '÷'];
     const [decimalAdded, setDecimalAdded] = useState(false);
+
+    const handleKeyDown = (e) => {
+        const key = e.key;
+        if (/\d/.test(key)) {
+            setInputVal(inputVal + key);
+        } else if (key === '*' || key === '/' || key === '+' || key === '-') {
+            // } else if (operators.indexOf(key) > -1) {
+            const lastChar = inputVal[inputVal.length - 1];
+            if (inputVal !== '' && operators.indexOf(lastChar) === -1) {
+                setInputVal(inputVal + key);
+            } else if (inputVal === '' && key === '-') {
+                setInputVal(key);
+            } else if (key === 'Backspace') {
+                const newInputVal = inputVal.slice(0, -1);
+                setInputVal(newInputVal);
+                setDecimalAdded(newInputVal.includes('.'));
+            }
+
+            if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+                setInputVal(inputVal.replace(/.$/, key));
+            }
+            setDecimalAdded(false);
+        } else if (key === '.') {
+            if (!decimalAdded) {
+                setInputVal(inputVal + key);
+                setDecimalAdded(true);
+            }
+        } else if (key === 'Backspace') {
+            const valueStr = String(inputVal);
+            if (valueStr?.length) {
+                setInputVal(valueStr.slice(0, -1));
+            } else {
+                setInputVal('');
+            }
+            // try {
+            //     const newInputVal = inputVal.slice(0, -1);
+            //     setInputVal(newInputVal);
+            //     setDecimalAdded(newInputVal.includes('.'));
+            // } catch (error) {
+            //     console.error(error);
+            //     setInputVal('Invalid value');
+            //     // Обработка ошибки, например:
+            //     // setInputVal('');
+            //     setDecimalAdded(false);
+            // }
+        } else if (key === 'Enter') {
+            let equation = inputVal;
+            // const lastChar = equation[equation.length - 1];
+            // if (typeof equation === 'string') {
+            //     equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+            // }
+            // if (operators.indexOf(lastChar) > -1 || lastChar === '.') {
+            //     equation = equation.replace(/.$/, '');
+            // }
+            try {
+                if (typeof equation === 'string') {
+                    equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+                }
+                const lastChar = equation[equation.length - 1];
+                if (operators.indexOf(lastChar) > -1 || lastChar === '.') {
+                    equation = equation.replace(/.$/, '');
+                }
+            } catch (error) {
+                console.error('An error occurred:', error);
+            }
+
+            // if (equation.indexOf('/0') !== -1) {
+            //     setInputVal('Error: Division by zero');
+            //     return;
+            // }
+            if (typeof equation === 'string' && equation.indexOf('/0') !== -1) {
+                setInputVal('Error: Division by zero');
+                return;
+            }
+            if (equation) {
+                console.log(typeof equation, equation);
+                setInputVal(math.evaluate(equation.toString()));
+            }
+            setDecimalAdded(false);
+        } else if (key === 'Escape') {
+            setInputVal('');
+            setDecimalAdded(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => document.removeEventListener('keydown', handleKeyDown);
+    });
 
     const handleClick = (e) => {
         const btnVal = e.target.innerHTML;
@@ -25,10 +116,10 @@ const Calculator = () => {
                 equation = equation.replace(/.$/, '');
             }
 
-            if (equation.indexOf('/0') !== -1 || typeof equation === 'string') {
-                setInputVal('Invalid action');
-                return;
-            }
+            // if (equation.indexOf('/0') !== -1 || typeof equation === 'string') {
+            //     setInputVal('Invalid action');
+            //     return;
+            // }
             // if (equation) {
             //     setInputVal(math.evaluate(equation));
             // }
